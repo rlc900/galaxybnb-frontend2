c2ea544c664e2cc6edd85fe27993d75c28647862
Rianna <rianna.cleary@gmail.com>
2020-07-02 09:16:07 -0400

    fixing font

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import './index.css'
 import GlobalFonts from './fonts/fonts.js'
 import MainContainer from './containers/MainContainer'
 
