210764741032fd0bff12181b2affa74a48d8e89e
Rianna <rianna.cleary@gmail.com>
2020-07-02 10:09:43 -0400

    fixed index css and js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './fonts/Starjedi.ttf'
 import './index.css';
 import 'semantic-ui-css/semantic.min.css'
 import App from './App';
